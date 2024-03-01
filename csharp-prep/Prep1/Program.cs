656e437da89b841607643536fe5b164a577ba077
Mickael <micrandr@gmail.com>
2024-03-01 21:43:16 +0300

    add comments

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,7 +3,15 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+    {        
+        //ask a user their first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // ask  
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        
+        Console.WriteLine($"Your name is {name}, {firstName} {name}");
     }
 }
