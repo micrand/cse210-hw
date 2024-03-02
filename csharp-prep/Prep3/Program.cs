f8a34eacc5f8e5f6a39fe5c235c561ba3271d9ee
Mickael <micrandr@gmail.com>
2024-03-02 14:43:57 +0300

    Learning Activity - C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        int magicNumber;
+        int userGuess;
+        
+        string response = "no";
+
+        Console.Write("What is the magic number? ");
+        magicNumber = int.Parse(Console.ReadLine());
+
+        do 
+        {
+           
+            Console.Write("What is your guess? ");
+            userGuess = int.Parse(Console.ReadLine());
+
+            string resultText="";
+
+            if(magicNumber < userGuess )
+            {
+                resultText = "Lower";
+                response = "yes";
+            }
+            else if( magicNumber > userGuess)
+            {
+                resultText = "Higher";
+                response = "yes";
+            }else {
+                resultText = "Bingo";
+                response = "no";
+
+            }
+
+            Console.WriteLine($"{resultText}");
+           
+        }
+        while( response == "yes");
     }
 }
