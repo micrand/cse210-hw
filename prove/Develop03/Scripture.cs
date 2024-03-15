52db82c14408dc66c4dfd02f3ca6e3b6b60e27af
Mickael <micrandr@gmail.com>
2024-03-15 08:29:08 +0300

    W03 - design activity

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,27 @@
+public class Scripture
+{
+    private string _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference Reference, string text)
+    {  
+
+
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return true;
+    }
+
+}
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,31 @@
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+
+    }
+
+    public void Hide()
+    {
+
+    }
+
+    public void Show()
+    {
+
+    }
+
+    public void isHidden()
+    {
+        
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+
+}
