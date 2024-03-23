1a3e569223b324b1be5a13379ca995e9654f0af8
Mickael <micrandr@gmail.com>
2024-03-23 16:03:15 +0300

    Encapsulation - Fraction program

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -5,19 +5,25 @@ class Fraction
 
     public Fraction()
     {
-        Console.WriteLine($"1/1;");
+        //Console.WriteLine($"1/1;");
+        _top = 1;
+        _bottom = 1;
         return;
     }
 
     public Fraction(int wholeNumber)
     {
-        Console.WriteLine($"{wholeNumber}/1;");
+        _top = wholeNumber;
+        _bottom = 1;
+        //Console.WriteLine($"{wholeNumber}/1;");
 
     }
 
     public Fraction(int top, int bottom)
     {
-        Console.WriteLine($"{top}/{bottom};");
+        _top = top;
+        _bottom = bottom;
+        //Console.WriteLine($"{top}/{bottom};");
     }
 
     public int GetTop()
@@ -38,5 +44,16 @@ class Fraction
     public void SetBottom(int bottom)
     {
         _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
     }
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -6,10 +6,20 @@ class Program
     {
         Fraction f1 = new Fraction();
         Fraction f2 = new Fraction(5);
-        Fraction f3 = new Fraction(3,4);        
+        Fraction f3 = new Fraction(3,4);   
+        Fraction f4 = new Fraction(1,3);
 
-        // Console.WriteLine(f1);
-        // Console.WriteLine(f2);
-        // Console.WriteLine(f3);
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Console.WriteLine(f3.GetFractionString());        
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
     }
 }
