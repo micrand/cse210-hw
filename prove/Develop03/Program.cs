40f464ade218819692afcfa2e6b3ec5efb52c6de
Mickael <micrandr@gmail.com>
2024-03-23 18:29:10 +0300

    change: w03 program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        const string spaceSeparator = " ";
+        string scriptureText = "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.";
+        // Word word = new Word(text);
+        Reference reference = new Reference("John",3,3);
+        Scripture scripture = new Scripture(reference, scriptureText);
+        string word = "";
+
+        string displayText = "";
+        while( displayText != "quit" )
+        {
+            int nbText = scriptureText.Split(spaceSeparator).Count();
+            var i = 0;
+            foreach(var data in scriptureText.Split(spaceSeparator))
+            {
+                Word displayWord = new Word(data);                
+                word = displayWord.GetDisplayText()+spaceSeparator;
+                Console.Write(word);
+            }
+            Console.WriteLine();
+            displayText = Console.ReadLine();
+            Console.Clear();
+        }
+
+        // Console.WriteLine(GetDisplayText());
     }
 }
